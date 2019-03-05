3af21f9f0742cd9f814355ecfce741d9fdf3334c
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-03-06 05:59:48 +0700

    tweek code

--- app/src/main/java/com/dracoo/jobreport/feature/dashboard/DashboardFragment.java
+++ app/src/main/java/com/dracoo/jobreport/feature/dashboard/DashboardFragment.java
@@ -10,17 +10,7 @@ import android.view.ViewGroup;
 
 import com.dracoo.jobreport.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link DashboardFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link DashboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DashboardFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
