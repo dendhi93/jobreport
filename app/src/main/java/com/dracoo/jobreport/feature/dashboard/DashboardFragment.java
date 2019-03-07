1d1e58228f14aa10e2fba461874c8bfbfc18a2f4
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-03-07 22:52:22 +0700

    tweek code

--- app/src/main/java/com/dracoo/jobreport/feature/dashboard/DashboardFragment.java
+++ app/src/main/java/com/dracoo/jobreport/feature/dashboard/DashboardFragment.java
@@ -57,7 +57,6 @@ public class DashboardFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
