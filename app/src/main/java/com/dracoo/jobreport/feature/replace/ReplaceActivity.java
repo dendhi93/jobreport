8152a786169d46a1f2c8adb36a7d9841c3b61b44
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-03-09 06:26:29 +0700

    prepare desain replace menu

---
+++ app/src/main/java/com/dracoo/jobreport/feature/replace/ReplaceActivity.java
@@ -0,0 +1,15 @@
+package com.dracoo.jobreport.feature.replace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dracoo.jobreport.R;
+
+public class ReplaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_replace);
+    }
+}
