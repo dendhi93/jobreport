3d9b79e667e6d19cb9625cfe2030c7162f7b327a
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-03-09 20:33:23 +0700

    prepare data m2m

---
+++ app/src/main/java/com/dracoo/jobreport/feature/datam2m/DataM2mActivity.java
@@ -0,0 +1,15 @@
+package com.dracoo.jobreport.feature.datam2m;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dracoo.jobreport.R;
+
+public class DataM2mActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_m2m);
+    }
+}
