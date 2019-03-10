b141e3a13a9b8ae7445ba84c35755c3bd880a256
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-03-11 05:51:28 +0700

    prepare design parameter conn

---
+++ app/src/main/java/com/dracoo/jobreport/feature/vsatparameter/ParameterActivity.java
@@ -0,0 +1,15 @@
+package com.dracoo.jobreport.feature.vsatparameter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dracoo.jobreport.R;
+
+public class ParameterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parameter);
+    }
+}
