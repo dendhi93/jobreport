79a2bcf5f8290cd65f9bb0790969c7ac48b563d8
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-03-11 05:38:04 +0700

    desain xpoll

---
+++ app/src/main/java/com/dracoo/jobreport/feature/xpoll/XpollActivity.java
@@ -0,0 +1,15 @@
+package com.dracoo.jobreport.feature.xpoll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dracoo.jobreport.R;
+
+public class XpollActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xpoll);
+    }
+}
