f4c08b134accc1916896fa8b85fa8f2d2e3245b5
deddy effendi <deddy.effendi@reebonz.com>
2019-03-12 11:02:23 +0700

    preparing problem Fragment

--- app/src/main/java/com/dracoo/jobreport/feature/problem/ProblemFragment.java
+++ app/src/main/java/com/dracoo/jobreport/feature/problem/ProblemFragment.java
@@ -1,7 +1,5 @@
 package com.dracoo.jobreport.feature.problem;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,11 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.dracoo.jobreport.R;
+import com.dracoo.jobreport.util.ConfigApps;
+import com.dracoo.jobreport.util.MessageUtils;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class ProblemFragment extends Fragment {
+
+    private MessageUtils messageUtils;
 
 
     @Override
@@ -25,6 +28,28 @@ public class ProblemFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        messageUtils = new MessageUtils(getActivity());
+    }
+
+    @OnClick(R.id.imgB_problem_submit)
+    void submitProblem (){
+        messageUtils.toastMessage("coba", ConfigApps.T_DEFAULT);
+    }
+
+    @OnClick(R.id.imgB_problem_cancel)
+    void probCancel(){
+        messageUtils.toastMessage("coba 2", ConfigApps.T_DEFAULT);
+    }
+
+
+
+
+
 
 
 
