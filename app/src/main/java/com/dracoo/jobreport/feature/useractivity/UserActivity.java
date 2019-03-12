1f280ee98b3e6dd54731adb70dd535c500f7f86c
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-03-12 21:57:02 +0700

    design dashboard, add user activity

---
+++ app/src/main/java/com/dracoo/jobreport/feature/useractivity/UserActivity.java
@@ -0,0 +1,15 @@
+package com.dracoo.jobreport.feature.useractivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dracoo.jobreport.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
