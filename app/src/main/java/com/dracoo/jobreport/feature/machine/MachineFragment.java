e51dba9efe1b0882eac4253efd614477f9eea357
deddy effendi <deddy.effendi@reebonz.com>
2019-03-13 15:50:54 +0700

    implement radiolistener in machine fragment, and correcting id in xml

--- app/src/main/java/com/dracoo/jobreport/feature/machine/MachineFragment.java
+++ app/src/main/java/com/dracoo/jobreport/feature/machine/MachineFragment.java
@@ -7,14 +7,43 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.dracoo.jobreport.R;
+import com.dracoo.jobreport.util.MessageUtils;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class MachineFragment extends Fragment {
 
+    @BindView(R.id.rg_machine_location)
+    RadioGroup rg_machine_location;
+
+    @BindView(R.id.rg_machine_qty)
+    RadioGroup rg_machine_qty;
+
+    @BindView(R.id.rg_machine_id)
+    RadioGroup rg_machine_id;
+
+    @BindView(R.id.rg_machine_24)
+    RadioGroup rg_machine_24;
+
+    private MessageUtils messageUtils;
+
+    private RadioButton rb_machineLocation;
+    private String selectedMachineLocation = "";
+
+    private RadioButton rb_machineQty;
+    private String selectedMachineQty = "";
+
+    private RadioButton rb_machineId;
+    private String selectedMachineId = "";
+
+    private RadioButton rb_machine24;
+    private String selectedMachine24 = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,6 +54,57 @@ public class MachineFragment extends Fragment {
 
         return view;
 
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        messageUtils = new MessageUtils(getActivity());
+        rbMachineLocation();
+        rbMachineQty();
+        rbMachineId();
+        rbMachine24();
+    }
+
+    private void rbMachineLocation(){
+        rg_machine_qty.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                rb_machineQty =  (RadioButton) getView().findViewById(i);
+                selectedMachineLocation = rb_machineQty.getText().toString();
+            }
+        });
+    }
+
+    private void rbMachineQty(){
+        rg_machine_location.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                rb_machineQty =  (RadioButton) getView().findViewById(i);
+                selectedMachineQty = rb_machineQty.getText().toString();
+            }
+        });
+    }
+
+    private void rbMachineId(){
+        rg_machine_id.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                rb_machineId =  (RadioButton) getView().findViewById(i);
+                selectedMachineId = rb_machineId.getText().toString();
+            }
+        });
+    }
+
+    private void rbMachine24(){
+        rg_machine_24.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                rb_machine24 =  (RadioButton) getView().findViewById(i);
+                selectedMachine24 = rb_machine24.getText().toString();
+            }
+        });
     }
 
 
