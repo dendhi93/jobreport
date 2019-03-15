30f9b9818f68f1421f744b1151f4e6430bc00108
deddy effendi <deddy.effendi@reebonz.com>
2019-03-15 17:18:40 +0700

    creating query transHist

--- app/src/main/java/com/dracoo/jobreport/database/adapter/TransHistoryAdapter.java
+++ app/src/main/java/com/dracoo/jobreport/database/adapter/TransHistoryAdapter.java
@@ -1,11 +1,13 @@
 package com.dracoo.jobreport.database.adapter;
 
 import android.content.Context;
+import android.database.Cursor;
 
 import com.dracoo.jobreport.database.master.MasterTransHistory;
 import com.j256.ormlite.dao.Dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class TransHistoryAdapter extends DatabaseAdapter {
     private Dao<MasterTransHistory, Integer> transHist;
@@ -19,5 +21,38 @@ public class TransHistoryAdapter extends DatabaseAdapter {
             transHist = getDao(MasterTransHistory.class);
         }
         return transHist;
+    }
+
+
+    public ArrayList<MasterTransHistory> load_trans(String custId, String un){
+        ArrayList<MasterTransHistory> transHists = new ArrayList<>();
+        Cursor cursor = null;
+
+        cursor = load_transCursor(custId, un);
+
+        while (cursor.moveToNext()) {
+            MasterTransHistory mTransHist = new MasterTransHistory();
+            mTransHist.setId_site(cursor.getInt(0));
+            mTransHist.setTrans_step(cursor.getString(1));
+
+            transHists.add(mTransHist);
+        }
+        cursor.close();
+        getReadableDatabase().close();
+        return transHists;
+    }
+
+    public Cursor load_transCursor(String custId, String un){
+        Cursor cursor;
+
+        String sql = "SELECT DISTINCT " +
+                                "id_trans, " + //0
+                                "trans_step " + //1
+                    "from t_trans_history " +
+                    " where id_site like " + custId + "' " +
+                    " and un_user = '" +un+ "' ";
+
+        cursor = getReadableDatabase().rawQuery(sql, null);
+        return cursor;
     }
 }
