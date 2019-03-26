ca338ee934b09b757583701bca60f0fb0f2c8dc1
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-03-27 06:06:07 +0700

    add query vsat and m2m replace

--- app/src/main/java/com/dracoo/jobreport/database/adapter/M2mReplaceAdapter.java
+++ app/src/main/java/com/dracoo/jobreport/database/adapter/M2mReplaceAdapter.java
@@ -1,11 +1,13 @@
 package com.dracoo.jobreport.database.adapter;
 
 import android.content.Context;
+import android.database.Cursor;
 
 import com.dracoo.jobreport.database.master.MasterM2mReplace;
 import com.j256.ormlite.dao.Dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class M2mReplaceAdapter extends DatabaseAdapter {
     private Dao<MasterM2mReplace, Integer> m2MReplace;
@@ -19,5 +21,37 @@ public class M2mReplaceAdapter extends DatabaseAdapter {
             m2MReplace = getDao(MasterM2mReplace.class);
         }
         return m2MReplace;
+    }
+
+    public ArrayList<MasterM2mReplace> val_m2mReplace(int custId, String un){
+        ArrayList<MasterM2mReplace> replaces = new ArrayList<>();
+        Cursor cursor = null;
+
+        cursor = val_m2mReplaceCursor(custId, un);
+
+        while (cursor.moveToNext()) {
+            MasterM2mReplace replace = new MasterM2mReplace();
+            replace.setId_replace(cursor.getInt(0));
+            replace.setId_site(cursor.getInt(1));
+
+            replaces.add(replace);
+        }
+        cursor.close();
+        getReadableDatabase().close();
+        return replaces;
+    }
+
+    public Cursor val_m2mReplaceCursor(int custId, String un){
+        Cursor cursor;
+
+        String sql = "SELECT id_replace " +
+                " id_site " +
+                "from m2m_setup " +
+                " where id_site = " + custId + " " +
+                " and un_user = '" +un+ "' ";
+
+
+        cursor = getReadableDatabase().rawQuery(sql, null);
+        return cursor;
     }
 }
--- app/src/main/java/com/dracoo/jobreport/database/adapter/VsatReplaceAdapter.java
+++ app/src/main/java/com/dracoo/jobreport/database/adapter/VsatReplaceAdapter.java
@@ -1,11 +1,13 @@
 package com.dracoo.jobreport.database.adapter;
 
 import android.content.Context;
+import android.database.Cursor;
 
 import com.dracoo.jobreport.database.master.MasterVsatReplace;
 import com.j256.ormlite.dao.Dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class VsatReplaceAdapter extends DatabaseAdapter {
     private Dao<MasterVsatReplace, Integer> vsatReplace;
@@ -19,5 +21,38 @@ public class VsatReplaceAdapter extends DatabaseAdapter {
             vsatReplace = getDao(MasterVsatReplace.class);
         }
         return vsatReplace;
+    }
+
+
+    public ArrayList<MasterVsatReplace> val_vsatReplace(int custId, String un){
+        ArrayList<MasterVsatReplace> replaces = new ArrayList<>();
+        Cursor cursor = null;
+
+        cursor = val_vsatReplaceCursor(custId, un);
+
+        while (cursor.moveToNext()) {
+            MasterVsatReplace replace = new MasterVsatReplace();
+            replace.setId_replace(cursor.getInt(0));
+            replace.setId_site(cursor.getInt(1));
+
+            replaces.add(replace);
+        }
+        cursor.close();
+        getReadableDatabase().close();
+        return replaces;
+    }
+
+    public Cursor val_vsatReplaceCursor(int custId, String un){
+        Cursor cursor;
+
+        String sql = "SELECT id_replace " +
+                " id_site " +
+                "from m2m_setup " +
+                " where id_site = " + custId + " " +
+                " and un_user = '" +un+ "' ";
+
+
+        cursor = getReadableDatabase().rawQuery(sql, null);
+        return cursor;
     }
 }
