2ed4eab557551e4c26b63f3bffe4ea12cfe2564d
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-03-27 21:09:01 +0700

    add validation at replace activity

--- app/src/main/java/com/dracoo/jobreport/feature/replace/ReplaceActivity.java
+++ app/src/main/java/com/dracoo/jobreport/feature/replace/ReplaceActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.dracoo.jobreport.R;
+import com.dracoo.jobreport.database.adapter.M2mReplaceAdapter;
+import com.dracoo.jobreport.database.adapter.VsatReplaceAdapter;
 import com.dracoo.jobreport.database.master.MasterM2mReplace;
 import com.dracoo.jobreport.database.master.MasterVsatReplace;
 import com.dracoo.jobreport.util.ConfigApps;
@@ -24,6 +27,39 @@ public class ReplaceActivity extends AppCompatActivity {
     LinearLayout ln_replace_vsat;
     @BindView(R.id.ln_replace_m2m)
     LinearLayout ln_replace_m2m;
+    //vsat
+    @BindView(R.id.txt_rep_vsatModem)
+    EditText txt_rep_vsatModem;
+    @BindView(R.id.txt_rep_vsatAdaptor)
+    EditText txt_rep_vsatAdaptor;
+    @BindView(R.id.txt_rep_vsatFh)
+    EditText txt_rep_vsatFh;
+    @BindView(R.id.txt_rep_vsatLnb)
+    EditText txt_rep_vsatLnb;
+    @BindView(R.id.txt_rep_vsatRfu)
+    EditText txt_rep_vsatRfu;
+
+    //m2m
+    @BindView(R.id.txt_rep_m2m_brand)
+    EditText txt_rep_m2m_brand;
+    @BindView(R.id.txt_rep_m2m_sn)
+    EditText txt_rep_m2m_sn;
+    @BindView(R.id.txt_rep_m2m_adaptorSn)
+    EditText txt_rep_m2m_adaptorSn;
+    @BindView(R.id.txt_rep_m2m_adaptorBrand)
+    EditText txt_rep_m2m_adaptorBrand;
+    @BindView(R.id.txt_rep_m2m_sc1Brand)
+    EditText txt_rep_m2m_sc1Brand;
+    @BindView(R.id.txt_rep_m2m_sc1Sn)
+    EditText txt_rep_m2m_sc1Sn;
+    @BindView(R.id.txt_rep_m2m_sc1puk)
+    EditText txt_rep_m2m_sc1puk;
+    @BindView(R.id.txt_rep_m2m_sc2Brand)
+    EditText txt_rep_m2m_sc2Brand;
+    @BindView(R.id.txt_rep_m2m_sc2Sn)
+    EditText txt_rep_m2m_sc2Sn;
+    @BindView(R.id.txt_rep_m2m_sc2puk)
+    EditText txt_rep_m2m_sc2puk;
 
     private MessageUtils messageUtils;
     private Preference preference;
@@ -51,34 +87,80 @@ public class ReplaceActivity extends AppCompatActivity {
         messageUtils = new MessageUtils(ReplaceActivity.this);
         preference = new Preference(ReplaceActivity.this);
         try{
-            intentConnectionType = getIntent().getIntExtra(EXTRA_CALLER_CONN, 0);
+            intentConnectionType = getIntent().getIntExtra(EXTRA_CALLER_CONN, 1);
         }catch (Exception e){ Log.d("ReplaceErr ",""+e.toString()); }
 
-        if (intentConnectionType == 1){
+        if (intentConnectionType == EXTRA_CALLER_VSATCONN){
             ln_replace_vsat.setVisibility(View.VISIBLE);
             ln_replace_m2m.setVisibility(View.GONE);
-        }else{
+        }else if (intentConnectionType == EXTRA_CALLER_M2MCONN){
             ln_replace_vsat.setVisibility(View.GONE);
             ln_replace_m2m.setVisibility(View.VISIBLE);
         }
 
-        if (getSupportActionBar() != null && intentConnectionType == 1){
+        if (getSupportActionBar() != null && intentConnectionType == EXTRA_CALLER_VSATCONN){
             getSupportActionBar().setSubtitle("VSAT");
-        }else if (getSupportActionBar() != null && intentConnectionType != 1){
+        }else if (getSupportActionBar() != null && intentConnectionType == EXTRA_CALLER_M2MCONN){
             getSupportActionBar().setSubtitle("M2M");
         }
+
+        try {
+            vsatReplaceDao = new VsatReplaceAdapter(getApplicationContext()).getAdapter();
+            m2mReplaceDao = new M2mReplaceAdapter(getApplicationContext()).getAdapter();
+        }catch (Exception e){}
 
     }
 
     @OnClick(R.id.imgB_rep_submit)
     void submitRep(){
-        messageUtils.toastMessage("coba", ConfigApps.T_DEFAULT);
+        if (intentConnectionType == EXTRA_CALLER_VSATCONN){
+            if (!vsatReplaceVal()){
+                messageUtils.snackBar_message(getString(R.string.emptyString), ReplaceActivity.this, ConfigApps.SNACKBAR_NO_BUTTON);
+            }else{
+
+            }
+        }else if (intentConnectionType == EXTRA_CALLER_M2MCONN){
+            if (!m2mReplaceVal()){
+                messageUtils.snackBar_message(getString(R.string.emptyString), ReplaceActivity.this, ConfigApps.SNACKBAR_NO_BUTTON);
+            }else{
+
+            }
+        }
     }
 
 
     @OnClick(R.id.imgB_rep_cancel)
     void cancelRep(){
-        messageUtils.toastMessage("coba2", ConfigApps.T_DEFAULT);
+        finish();
+    }
+
+    public boolean vsatReplaceVal(){
+        if(txt_rep_vsatModem.getText().toString().trim().equals("") ||
+                txt_rep_vsatAdaptor.getText().toString().trim().equals("") ||
+                txt_rep_vsatFh.getText().toString().trim().equals("")||
+                txt_rep_vsatLnb.getText().toString().trim().equals("") ||
+                txt_rep_vsatRfu.getText().toString().trim().equals("")){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean m2mReplaceVal(){
+        if (txt_rep_m2m_brand.getText().toString().trim().equals("")||
+                txt_rep_m2m_sn.getText().toString().trim().equals("")||
+                txt_rep_m2m_adaptorBrand.getText().toString().trim().equals("") ||
+                txt_rep_m2m_adaptorSn.getText().toString().trim().equals("") ||
+                txt_rep_m2m_sc1Brand.getText().toString().equals("") ||
+                txt_rep_m2m_sc1Sn.getText().toString().equals("") ||
+                txt_rep_m2m_sc1puk.getText().toString().trim().equals("") ||
+                txt_rep_m2m_sc2Brand.getText().toString().trim().equals("") ||
+                txt_rep_m2m_sc2Sn.getText().toString().trim().equals("") ||
+                txt_rep_m2m_sc2puk.getText().toString().trim().equals("")){
+            return false;
+        }else{
+            return true;
+        }
     }
 
     @Override
