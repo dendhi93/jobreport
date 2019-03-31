0c8bfeeafdf778cc84f27e52b2fd92d895b82741
dendhi93 <44012078+dendhi93@users.noreply.github.com>
2019-03-31 20:20:48 +0700

    redesign parameter activity, add query connection parameter adapter

--- app/src/main/java/com/dracoo/jobreport/database/adapter/ConnectionParameterAdapter.java
+++ app/src/main/java/com/dracoo/jobreport/database/adapter/ConnectionParameterAdapter.java
@@ -1,11 +1,13 @@
 package com.dracoo.jobreport.database.adapter;
 
 import android.content.Context;
+import android.database.Cursor;
 
 import com.dracoo.jobreport.database.master.MasterConnectionParameter;
 import com.j256.ormlite.dao.Dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ConnectionParameterAdapter extends DatabaseAdapter {
     private Dao<MasterConnectionParameter, Integer> connParameter;
@@ -19,5 +21,36 @@ public class ConnectionParameterAdapter extends DatabaseAdapter {
             connParameter = getDao(MasterConnectionParameter.class);
         }
         return connParameter;
+    }
+
+    public ArrayList<MasterConnectionParameter> val_param(int custId, String un){
+        ArrayList<MasterConnectionParameter> params = new ArrayList<>();
+        Cursor cursor = null;
+
+        cursor = val_paramCursor(custId, un);
+
+        while (cursor.moveToNext()) {
+            MasterConnectionParameter param = new MasterConnectionParameter();
+            param.setId_parameter(cursor.getInt(0));
+            param.setId_site(cursor.getInt(1));
+
+            params.add(param);
+        }
+        cursor.close();
+        getReadableDatabase().close();
+        return params;
+    }
+
+    public Cursor val_paramCursor(int custId, String un){
+        Cursor cursor;
+
+        String sql = "SELECT id_parameter, " +
+                " id_site " +
+                "from network_parameter " +
+                " where id_site = " + custId + " " +
+                " and un_user = '" +un+ "' ";
+
+        cursor = getReadableDatabase().rawQuery(sql, null);
+        return cursor;
     }
 }
--- app/src/main/java/com/dracoo/jobreport/feature/vsatparameter/ParameterActivity.java
+++ app/src/main/java/com/dracoo/jobreport/feature/vsatparameter/ParameterActivity.java
@@ -3,12 +3,19 @@ package com.dracoo.jobreport.feature.vsatparameter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.dracoo.jobreport.R;
+import com.dracoo.jobreport.database.adapter.ConnectionParameterAdapter;
+import com.dracoo.jobreport.database.adapter.TransHistoryAdapter;
+import com.dracoo.jobreport.database.master.MasterConnectionParameter;
+import com.dracoo.jobreport.database.master.MasterTransHistory;
 import com.dracoo.jobreport.util.ConfigApps;
 import com.dracoo.jobreport.util.MessageUtils;
+import com.dracoo.jobreport.util.Preference;
+import com.j256.ormlite.dao.Dao;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,16 +25,39 @@ public class ParameterActivity extends AppCompatActivity {
 
     @BindView(R.id.rg_par_subnetmask)
     RadioGroup rg_par_subnetmask;
-    @BindView(R.id.rb_param_sub1)
-    RadioButton rb_param_sub1;
-    @BindView(R.id.rb_param_sub2)
-    RadioButton rb_param_sub2;
-    @BindView(R.id.rb_param_sub3)
-    RadioButton rb_param_sub3;
+    @BindView(R.id.txt_parameter_ipLLan)
+    EditText txt_parameter_ipLLan;
+    @BindView(R.id.txt_parameter_long)
+    EditText txt_parameter_long;
+    @BindView(R.id.txt_parameter_symRate)
+    EditText txt_parameter_symRate;
+    @BindView(R.id.txt_parameter_freq)
+    EditText txt_parameter_freq;
+    @BindView(R.id.txt_parameter_esn)
+    EditText txt_parameter_esn;
+    @BindView(R.id.txt_parameter_gateway)
+    EditText txt_parameter_gateway;
+    @BindView(R.id.txt_parameter_snmp)
+    EditText txt_parameter_snmp;
+    @BindView(R.id.txt_parameter_signal)
+    EditText txt_parameter_signal;
+    @BindView(R.id.txt_parameter_dataRate)
+    EditText txt_parameter_dataRate;
+    @BindView(R.id.txt_parameter_fec)
+    EditText txt_parameter_fec;
+    @BindView(R.id.txt_parameter_powSetting)
+    EditText txt_parameter_powSetting;
+    @BindView(R.id.txt_parameter_esNo)
+    EditText txt_parameter_esNo;
+    @BindView(R.id.txt_parameter_cNo)
+    EditText txt_parameter_cNo;
 
-    private String selectedParameter = "";
+    private String selectedParameter = "null";
     private RadioButton rbParameter;
     private MessageUtils messageUtils;
+    private Preference preference;
+    private Dao<MasterConnectionParameter, Integer> connParamAdapter;
+    private Dao<MasterTransHistory, Integer> transHistAdapter;
 
 
     @Override
@@ -46,7 +76,12 @@ public class ParameterActivity extends AppCompatActivity {
         super.onStart();
 
         messageUtils = new MessageUtils(ParameterActivity.this);
+        preference = new Preference(ParameterActivity.this);
         displayParamRadio();
+        try{
+            connParamAdapter = new ConnectionParameterAdapter(getApplicationContext()).getAdapter();
+            transHistAdapter = new TransHistoryAdapter(getApplicationContext()).getAdapter();
+        }catch (Exception e){}
 
     }
 
@@ -54,27 +89,41 @@ public class ParameterActivity extends AppCompatActivity {
         rg_par_subnetmask.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
-//                rbParameter =  findViewById(i);
-//                selectedParameter = ""+rbParameter.getText().toString();
-                if (i == R.id.rb_param_sub1){
-                    selectedParameter = ""+rb_param_sub1.getText().toString();
-                }else if (i == R.id.rb_param_sub2){
-                    selectedParameter = ""+rb_param_sub2.getText().toString();
-                }else if (i == R.id.rb_param_sub3){
-                    selectedParameter = ""+rb_param_sub3.getText().toString();
-                }
+                rbParameter =  findViewById(i);
+                selectedParameter = ""+rbParameter.getText().toString();
             }
         });
     }
 
     @OnClick(R.id.imgB_par_submit)
     void submitPar(){
-        if (!rb_param_sub1.isChecked() || !rb_param_sub2.isChecked() || !rb_param_sub3.isChecked()){
-            messageUtils.toastMessage("Mohon dipilih pilhan pada kolom SubnetMask", ConfigApps.T_WARNING);
-        }else{
+        if (selectedParameter.equals("null") || selectedParameter == null){
+            messageUtils.snackBar_message("Mohon dipilih pilhan pada kolom SubnetMask", ParameterActivity.this,ConfigApps.SNACKBAR_NO_BUTTON);
+        }else if (!valEmptyText()){
+            messageUtils.snackBar_message(getString(R.string.emptyString), ParameterActivity.this,ConfigApps.SNACKBAR_NO_BUTTON);
+        } else{
             messageUtils.toastMessage("coba", ConfigApps.T_DEFAULT);
         }
+    }
 
+    private boolean valEmptyText(){
+        if (txt_parameter_ipLLan.getText().toString().trim().equals("") ||
+                txt_parameter_long.getText().toString().trim().equals("") ||
+                txt_parameter_symRate.getText().toString().trim().equals("") ||
+                txt_parameter_freq.getText().toString().trim().equals("") ||
+                txt_parameter_esn.getText().toString().trim().equals("") ||
+                txt_parameter_gateway.getText().toString().trim().equals("") ||
+                txt_parameter_snmp.getText().toString().trim().equals("") ||
+                txt_parameter_signal.getText().toString().trim().equals("") ||
+                txt_parameter_dataRate.getText().toString().trim().equals("") ||
+                txt_parameter_fec.getText().toString().trim().equals("") ||
+                txt_parameter_powSetting.getText().toString().trim().equals("") ||
+                txt_parameter_esNo.getText().toString().trim().equals("") ||
+                txt_parameter_cNo.getText().toString().trim().equals("")){
+            return false;
+        }else{
+            return true;
+        }
     }
 
 
