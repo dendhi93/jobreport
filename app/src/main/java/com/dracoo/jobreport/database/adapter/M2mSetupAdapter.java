65175dbf11f8ec70167ba87e577043e4691efd99
deddy effendi <deddy.effendi@reebonz.com>
2019-04-16 10:47:29 +0700

    tuning m2m setup query

--- app/src/main/java/com/dracoo/jobreport/database/adapter/M2mSetupAdapter.java
+++ app/src/main/java/com/dracoo/jobreport/database/adapter/M2mSetupAdapter.java
@@ -34,6 +34,17 @@ public class M2mSetupAdapter extends DatabaseAdapter {
             MasterM2mSetup setup = new MasterM2mSetup();
             setup.setId_setup(cursor.getInt(0));
             setup.setId_site(cursor.getInt(1));
+            setup.setBrand_type_m2m(cursor.getString(2));
+            setup.setSn_m2m(cursor.getString(3));
+            setup.setBrand_type_adaptor(cursor.getString(4));
+            setup.setSn_adaptor(cursor.getString(5));
+            setup.setSim_card1_type(cursor.getString(6));
+            setup.setSim_card1_sn(cursor.getString(7));
+            setup.setSim_card1_puk(cursor.getString(8));
+            setup.setSim_card2_type(cursor.getString(9));
+            setup.setSim_card2_sn(cursor.getString(10));
+            setup.setSim_card2_puk(cursor.getString(11));
+
 
             setups.add(setup);
         }
@@ -45,8 +56,18 @@ public class M2mSetupAdapter extends DatabaseAdapter {
     public Cursor val_m2mSetupCursor(int custId, String un){
         Cursor cursor;
 
-        String sql = "SELECT id_setup, " +
-                " id_site " +
+        String sql = "SELECT id_setup, " + //0
+                            " id_site, " + //1
+                            " brand_type_m2m, " + //2
+                            " sn_m2m, " + //3
+                            " brand_type_adaptor, " + //4
+                            " sn_adaptor, " + //5
+                            " sim_card1_type, " + //6
+                            " sim_card1_sn, " + //7
+                            " sim_card1_puk, " + //8
+                            " sim_card2_type, " + //9
+                            " sim_card2_sn, " + //10
+                            " sim_card2_puk " + //11
                 "from m2m_setup " +
                 " where id_site = " + custId + " " +
                 " and un_user = '" +un+ "' ";
