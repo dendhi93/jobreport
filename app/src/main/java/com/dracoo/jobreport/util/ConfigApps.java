54509149bce0b8ef77841a53805f13a89ff4bc84
dracoo <dendhi.fendi@gmail.com>
2019-08-15 09:57:53 +0700

    add gform input

--- app/src/main/java/com/dracoo/jobreport/util/ConfigApps.java
+++ app/src/main/java/com/dracoo/jobreport/util/ConfigApps.java
@@ -35,8 +35,8 @@ public class ConfigApps {
     public static final int GFORM_TYPE = 3;
     public static final int SUBMIT_SEND = 1;
 
-    //url gform
-    public static final String gformUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfL9LGNUtSCi_uh-cQjS644D1zpuzZahDwrXrGU8vA0Mbk-9w/viewform";
+    //url gform v1
+    public static final String gformUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfL9LGNUtSCi_uh-cQjS644D1zpuzZahDwrXrGU8vA0Mbk-9w/formResponse";
 
     //customer input
     public static final String techNameInput = "entry.338960680";
@@ -51,8 +51,78 @@ public class ConfigApps {
     public static final String proviencyInput = "entry.1227901858";
     public static final String latInput = "entry.1593799458";
     public static final String longitudeInput = "entry.726970911";
+    public static final String connTypeInput = "entry.2032056659";
+    public static final String progressInput = "entry.651734487";
+    public static final String takeOffInput = "entry.2084920842";
+    public static final String landingInput = "entry.2082939153";
+    public static final String pendingStartInput = "entry.904556259";
+    public static final String reasonPendingStartInput = "entry.783700454";
+    public static final String startInput = "entry.2050121004";
+    public static final String activityPendingInput = "entry.1901692711";
+    public static final String reasonActivityPendingInput = "entry.800006749";
+    public static final String restartInput = "entry.1535730819";
+    public static final String picInput = "entry.230823196";
+    public static final String chooseDeviceInput = "entry.1547303458";
 
+    //vsat conn
+    public static final String vsatProbInput = "entry.839003990";
+    public static final String modemVsatInput = "entry.56164180";
+    public static final String adaptorVsatInput = "entry.1476324065";
+    public static final String dipIDUInput = "entry.1904588505";
+    public static final String rfuInput = "entry.107133898";
+    public static final String lnbInput = "entry.152202183";
+    public static final String xModemVsatInput = "entry.1314192515";
+    public static final String xAdaptorVsatInput = "entry.1248459208";
+    public static final String xDipIDUInput = "entry.1113739701";
+    public static final String xRFUInput = "entry.863282643";
+    public static final String xLNBInput = "entry.1280121308";
+    public static final String xDipODUInput = "entry.589072077";
+    public static final String dateXpollInput = "entry.1214649736";
+    public static final String satteliteXpollInput = "entry.494581243";
+    public static final String cpiXpollInput = "entry.2129211258";
+    public static final String cnXpollInput = "entry.634103523";
+    public static final String asiXpollInput = "entry.1432626246";
+    public static final String picXpollInput = "entry.1364940602";
 
+    //m2m conn
+    public static final String probM2mInput = "entry.997578387";
+    public static final String modemM2mInput = "entry.1888636074";
+    public static final String adaptorM2mInput = "entry.201572360";
+    public static final String sim1ProvM2mInput = "entry.367196654";
+    public static final String sim1NoM2mInput = "entry.1552421323";
+    public static final String sim1PUKM2mInput = "entry.2048173561";
+    public static final String sim2ProvM2mInput = "entry.2125918833";
+    public static final String sim2NoM2mInput = "entry.176684191";
+    public static final String sim2PUKM2mInput = "entry.398787598";
+    public static final String xModemM2mInput = "entry.490460726";
+    public static final String xAdaptorM2mInput = "entry.1724095430";
+    public static final String xSim1ProvM2mInput = "entry.1736752098";
+    public static final String xSim1NoM2mInput = "entry.1745621664";
+    public static final String xSim1PukM2mInput = "entry.2014361105";
+    public static final String xSim2ProvM2mInput = "entry.1324318973";
+    public static final String xSim2NoM2mInput = "entry.682073080";
+    public static final String xSim2PukM2mInput = "entry.1920465534";
 
+    public static final String xActionInput = "entry.353195564";
+    public static final String resultInput = "entry.1041449695";
+    public static final String notesInput = "entry.1620393634";
 
+//========================================
+
+    //url gform v2
+    public static final String gformUrl_v2 = "https://docs.google.com/forms/d/e/1FAIpQLSez80JkKJOjqaqha2rhidv81U-Mdpc35OHbunVYR--Qm5tSGQ/formResponse";
+
+    public static final String techNameInputv2 = "entry.821468160";
+    public static final String serviceInputv2 = "entry.1471984630";
+    public static final String ttwoInputv2 = "entry.37351837";
+    public static final String remoteNameInputv2 = "entry.1011348814";
+    public static final String customerInputv2 = "entry.2092067474";
+    public static final String remoteLocationInputv2 = "entry.937632020";
+    public static final String addressInputv2 = "entry.1299654413";
+    public static final String cityInputv2 = "entry.73841220";
+    public static final String kabupatenInputv2 = "entry.746749290";
+    public static final String provInputv2 = "entry.2084070345";
+    public static final String latInputv2 = "entry.472133480";
+    public static final String longitudeInputv2 = "entry.1590897902";
+    public static final String connTypeInputv2 = "entry.1256810734";
 }
